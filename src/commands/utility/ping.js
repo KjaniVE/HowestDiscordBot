b74b8c775e90c104ab11a15751a344f1f02d2497
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-02-18 19:11:19 +0100

    improved ping command to actually see the ping of the bot

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,10 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+	async execute(interaction, client) {
+		const embed = new EmbedBuilder()
+			.setTitle('Pong!')
+			.setDescription(`API latency: ${Math.round(client.ws.ping)}ms`)
+			.setColor(0x2ecc71);
+
+		await interaction.reply({ embeds: [embed] });
+	}
 };
