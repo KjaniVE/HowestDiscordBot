b9d00b8beb4fbbc6295e275476a9995b03db8e53
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-02-19 11:03:13 +0100

    when a user leaves, it gets added to the logs

---
+++ src/events/userLeave.js
@@ -0,0 +1,22 @@
+const { Events } = require('discord.js');
+const { dbClient } = require('../dbClient');
+
+module.exports = {
+    name: Events.GuildMemberRemove,
+    once: false,
+    async execute(member) {
+        console.log('User left');
+        const user = member.user;
+        const discordId = user.id;
+        const username = user.username;
+
+        // Log the user joining the server with discord_id
+        const actionType = 'User left';
+        const description = `${username} left the server`;
+
+        await dbClient.query(
+            'INSERT INTO logs (action_type, action_description, discord_id) VALUES ($1, $2, $3)',
+            [actionType, description, discordId]
+        );
+    },
+};
