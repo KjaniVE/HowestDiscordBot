ac305c015e4d3cf3c6dda34aaea536ce1674b856
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-02-19 18:34:56 +0100

    eslint fix

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,67 +1,68 @@
-const { SlashCommandBuilder, MessageFlags, PermissionsBitField} = require('discord.js');
-const { logAction } = require("../../handlers/logHandler");
+const { SlashCommandBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+const { logAction } = require('../../handlers/logHandler');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Bans a user.')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to ban.')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for the ban.')
-                .setRequired(false)),
+	data: new SlashCommandBuilder()
+		.setName('ban')
+		.setDescription('Bans a user.')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('The user to ban.')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('The reason for the ban.')
+				.setRequired(false)),
 
-    async execute(interaction) {
-        const userToBan = interaction.options.getUser('user');
-        const memberToBan = interaction.guild.members.cache.get(userToBan.id);
-        const reason = interaction.options.getString('reason') || 'No reason provided.';
+	async execute(interaction) {
+		const userToBan = interaction.options.getUser('user');
+		const memberToBan = interaction.guild.members.cache.get(userToBan.id);
+		const reason = interaction.options.getString('reason') || 'No reason provided.';
 
-        if (userToBan.id === interaction.guild.ownerId) {
-            await logAction(interaction.guild, 'USER_BAN_FAILED', "USER IS OWNER", userToBan.id);
-            userToBan.send(`${interaction.user.username} tried banning you from your own server.`);
-            return interaction.reply({ content: 'You cannot ban the owner of the server. Owner has been notified', flags: MessageFlags.Ephemeral });
-        }
+		if (userToBan.id === interaction.guild.ownerId) {
+			await logAction(interaction.guild, 'USER_BAN_FAILED', 'USER IS OWNER', userToBan.id);
+			userToBan.send(`${interaction.user.username} tried banning you from your own server.`);
+			return interaction.reply({ content: 'You cannot ban the owner of the server. Owner has been notified', flags: MessageFlags.Ephemeral });
+		}
 
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            await logAction(interaction.guild, 'USER_BAN_FAILED', "NO PERMISSION TO KICK", interaction.user.id);
-            return interaction.reply({ content: 'You do not have permission to ban members.', flags: MessageFlags.Ephemeral });
-        }
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+			await logAction(interaction.guild, 'USER_BAN_FAILED', 'NO PERMISSION TO KICK', interaction.user.id);
+			return interaction.reply({ content: 'You do not have permission to ban members.', flags: MessageFlags.Ephemeral });
+		}
 
-        if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return interaction.reply({ content: "I don't have permission to ban members.", flags: MessageFlags.Ephemeral });
-        }
+		if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+			return interaction.reply({ content: 'I don\'t have permission to ban members.', flags: MessageFlags.Ephemeral });
+		}
 
-        if (!memberToBan.bannable) {
-            await logAction(interaction.guild, 'USER_BAN_FAILED', `USER ${userToBan.username} NOT BANNABLE`, interaction.user.id);
-            return interaction.reply({ content: `I cannot ban ${userToBan.username}. Insufficient permissions.`, flags: MessageFlags.Ephemeral });
-        }
+		if (!memberToBan.bannable) {
+			await logAction(interaction.guild, 'USER_BAN_FAILED', `USER ${userToBan.username} NOT BANNABLE`, interaction.user.id);
+			return interaction.reply({ content: `I cannot ban ${userToBan.username}. Insufficient permissions.`, flags: MessageFlags.Ephemeral });
+		}
 
-        if (userToBan.id === interaction.user.id) {
-            await logAction(interaction.guild, 'USER_BAN_FAILED', "USER TRIED TO KICK THEMSELVES", userToBan.id);
-            return interaction.reply({ content: 'You cannot ban yourself.', flags: MessageFlags.Ephemeral });
-        }
+		if (userToBan.id === interaction.user.id) {
+			await logAction(interaction.guild, 'USER_BAN_FAILED', 'USER TRIED TO KICK THEMSELVES', userToBan.id);
+			return interaction.reply({ content: 'You cannot ban yourself.', flags: MessageFlags.Ephemeral });
+		}
 
-        if (userToBan.bot) {
-            await logAction(interaction.guild, 'USER_BAN_FAILED', "USER IS A BOT", userToBan.id);
-            return interaction.reply({ content: 'You cannot ban a bot.', flags: MessageFlags.Ephemeral });
-        }
+		if (userToBan.bot) {
+			await logAction(interaction.guild, 'USER_BAN_FAILED', 'USER IS A BOT', userToBan.id);
+			return interaction.reply({ content: 'You cannot ban a bot.', flags: MessageFlags.Ephemeral });
+		}
 
-        try {
-            await userToBan.send(`You have been banned from ${interaction.guild.name} for the following reason: ${reason}`);
+		try {
+			await userToBan.send(`You have been banned from ${interaction.guild.name} for the following reason: ${reason}`);
 
-            await memberToBan.ban({reason: reason});
+			await memberToBan.ban({ reason: reason });
 
-            // 9. Confirm the kick in the channel
-            await interaction.reply({ content: `Successfully banend ${userToBan.tag}.`, flags: MessageFlags.Ephemeral });
+			// 9. Confirm the kick in the channel
+			await interaction.reply({ content: `Successfully banend ${userToBan.tag}.`, flags: MessageFlags.Ephemeral });
 
-            // 10. Log the kick action
-            await logAction(interaction.guild, 'USER_BANNED', reason, userToBan.id);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while trying to ban this user.', flags: MessageFlags.Ephemeral });
-        }
-    }
+			// 10. Log the kick action
+			await logAction(interaction.guild, 'USER_BANNED', reason, userToBan.id);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while trying to ban this user.', flags: MessageFlags.Ephemeral });
+		}
+	},
 };
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,75 +1,76 @@
-const { SlashCommandBuilder, MessageFlags, PermissionsBitField} = require('discord.js');
-const { logAction } = require("../../handlers/logHandler");
+const { SlashCommandBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+const { logAction } = require('../../handlers/logHandler');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('kick')
-        .setDescription('Kicks a user.')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to kick.')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('The reason for the kick.')
-                .setRequired(false)),
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kicks a user.')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('The user to kick.')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('The reason for the kick.')
+				.setRequired(false)),
 
-    async execute(interaction) {
-        const userToKick = interaction.options.getUser('user');
-        const memberToKick = interaction.guild.members.cache.get(userToKick.id);
-        const reason = interaction.options.getString('reason') || 'No reason provided.';
+	async execute(interaction) {
+		const userToKick = interaction.options.getUser('user');
+		const memberToKick = interaction.guild.members.cache.get(userToKick.id);
+		const reason = interaction.options.getString('reason') || 'No reason provided.';
 
-        // 1. Check if the user is the server owner
-        if (userToKick.id === interaction.guild.ownerId) {
-            await logAction(interaction.guild, 'USER_KICK_FAILED', "USER IS OWNER", userToKick.id);
-            userToKick.send(`${interaction.user.username} tried kicking you from your own server.`);
-            return interaction.reply({ content: 'You cannot kick the owner of the server. Owner has been notified', flags: MessageFlags.Ephemeral });
-        }
+		// 1. Check if the user is the server owner
+		if (userToKick.id === interaction.guild.ownerId) {
+			await logAction(interaction.guild, 'USER_KICK_FAILED', 'USER IS OWNER', userToKick.id);
+			userToKick.send(`${interaction.user.username} tried kicking you from your own server.`);
+			return interaction.reply({ content: 'You cannot kick the owner of the server. Owner has been notified', flags: MessageFlags.Ephemeral });
+		}
 
-        // 2. Check if the command user has permission to kick members
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
-            await logAction(interaction.guild, 'USER_KICK_FAILED', "NO PERMISSION TO KICK", interaction.user.id);
-            return interaction.reply({ content: 'You do not have permission to kick members.', flags: MessageFlags.Ephemeral });
-        }
+		// 2. Check if the command user has permission to kick members
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+			await logAction(interaction.guild, 'USER_KICK_FAILED', 'NO PERMISSION TO KICK', interaction.user.id);
+			return interaction.reply({ content: 'You do not have permission to kick members.', flags: MessageFlags.Ephemeral });
+		}
 
-        // 3. Check if the bot has permission to kick members
-        if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.KickMembers)) {
-            return interaction.reply({ content: "I don't have permission to kick members.", flags: MessageFlags.Ephemeral });
-        }
+		// 3. Check if the bot has permission to kick members
+		if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+			return interaction.reply({ content: 'I don\'t have permission to kick members.', flags: MessageFlags.Ephemeral });
+		}
 
-        // 4. Check if the user to kick is higher in role hierarchy or not kickable
-        if (!memberToKick.kickable) {
-            await logAction(interaction.guild, 'USER_KICK_FAILED', `USER ${userToKick.username} NOT KICKABLE`, interaction.user.id);
-            return interaction.reply({ content: `I cannot kick ${userToKick.username}. Insufficient permissions.`, flags: MessageFlags.Ephemeral });
-        }
+		// 4. Check if the user to kick is higher in role hierarchy or not kickable
+		if (!memberToKick.kickable) {
+			await logAction(interaction.guild, 'USER_KICK_FAILED', `USER ${userToKick.username} NOT KICKABLE`, interaction.user.id);
+			return interaction.reply({ content: `I cannot kick ${userToKick.username}. Insufficient permissions.`, flags: MessageFlags.Ephemeral });
+		}
 
-        // 5. Check if the user is trying to kick themselves
-        if (userToKick.id === interaction.user.id) {
-            await logAction(interaction.guild, 'USER_KICK_FAILED', "USER TRIED TO KICK THEMSELVES", userToKick.id);
-            return interaction.reply({ content: 'You cannot kick yourself.', flags: MessageFlags.Ephemeral });
-        }
+		// 5. Check if the user is trying to kick themselves
+		if (userToKick.id === interaction.user.id) {
+			await logAction(interaction.guild, 'USER_KICK_FAILED', 'USER TRIED TO KICK THEMSELVES', userToKick.id);
+			return interaction.reply({ content: 'You cannot kick yourself.', flags: MessageFlags.Ephemeral });
+		}
 
-        // 6. Check if the target is a bot
-        if (userToKick.bot) {
-            await logAction(interaction.guild, 'USER_KICK_FAILED', "USER IS A BOT", userToKick.id);
-            return interaction.reply({ content: 'You cannot kick a bot.', flags: MessageFlags.Ephemeral });
-        }
+		// 6. Check if the target is a bot
+		if (userToKick.bot) {
+			await logAction(interaction.guild, 'USER_KICK_FAILED', 'USER IS A BOT', userToKick.id);
+			return interaction.reply({ content: 'You cannot kick a bot.', flags: MessageFlags.Ephemeral });
+		}
 
-        try {
-            // 7. Send a DM to the user about the kick
-            await userToKick.send(`You have been kicked from ${interaction.guild.name} for the following reason: ${reason}`);
+		try {
+			// 7. Send a DM to the user about the kick
+			await userToKick.send(`You have been kicked from ${interaction.guild.name} for the following reason: ${reason}`);
 
-            // 8. Kick the user with the reason
-            await memberToKick.kick(reason);
+			// 8. Kick the user with the reason
+			await memberToKick.kick(reason);
 
-            // 9. Confirm the kick in the channel
-            await interaction.reply({ content: `Successfully kicked ${userToKick.tag}.`, flags: MessageFlags.Ephemeral });
+			// 9. Confirm the kick in the channel
+			await interaction.reply({ content: `Successfully kicked ${userToKick.tag}.`, flags: MessageFlags.Ephemeral });
 
-            // 10. Log the kick action
-            await logAction(interaction.guild, 'USER_KICKED', reason, userToKick.id);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while trying to kick this user.', flags: MessageFlags.Ephemeral });
-        }
-    }
+			// 10. Log the kick action
+			await logAction(interaction.guild, 'USER_KICKED', reason, userToKick.id);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while trying to kick this user.', flags: MessageFlags.Ephemeral });
+		}
+	},
 };
